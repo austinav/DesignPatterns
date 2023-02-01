fc4857dece55e87e09d9e367c6b13457cf87f69b
Austin <1627989+austinav@users.noreply.github.com>
2023-01-31 23:11:43 -0600

    Command and Strategy patterns

---
+++ Patterns/Strategy/Duck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.Strategy
+{
+	public abstract class Duck
+	{
+		public IFly FlyBehavior { private get; set; }
+		public IQuack QuackBehavior { private get; set; }
+
+		public Duck() { }
+
+		public abstract void display();
+
+		public void performFly() => FlyBehavior.fly();
+
+		public void performQuack() => QuackBehavior.quack();
+
+		public void swim() => Console.WriteLine("All ducks float, even decoys!");
+	}
+}
---
+++ Patterns/Strategy/QuackBehaviors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.Strategy
+{
+	public class Quack : IQuack
+	{
+		public void quack()
+		{
+			Console.WriteLine("Quack");
+		}
+	}
+
+	public class MuteQuack : IQuack
+	{
+		public void quack()
+		{
+			Console.WriteLine("<< Silence >>");
+		}
+	}
+
+	public class Squeak : IQuack
+	{
+		public void quack()
+		{
+			Console.WriteLine("Squeak");
+		}
+	}
+
+
+	public class FakeQuack : IQuack
+	{
+		public void quack()
+		{
+			Console.WriteLine("Qwak");
+		}
+	}
+}
